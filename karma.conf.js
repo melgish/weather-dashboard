300e07148848307be0f59614791fd5884fed9d22
Nicholas J. Fiorello Jr <nfiorello@yahoo.com>
2017-06-27 22:15:04 -0400

    adding test runner

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+// Karma configuration
+// Generated on Tue Jun 27 2017 21:22:05 GMT-0400 (Eastern Daylight Time)
+require('babel-register')();
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'client/**/*.spec.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'client/**/*.js': ['webpack']
+    },
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'text-summary' },
+        // { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
+        // { type: 'cobertura', subdir: '.', file: 'cobertura-coverage.xml' },
+        { type: 'html', subdir: 'www' },
+      ],
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_WARN,
+    autoWatch: true,
+    browsers: ['jsdom'],
+    singleRun: true,
+    concurrency: Infinity,
+    webpack: require('./webpack.config.test').default,
+    webpackMiddleware: {
+      stats: 'errors-only',
+    },
+  })
+}
---
+++ webpack.config.test.js
@@ -0,0 +1,57 @@
+import path from 'path';
+export default {
+  devtool: 'inline-source-map',
+  entry: {
+    main: './client/index',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ng-annotate-loader',
+            options: {
+              single_quotes: true,
+            },
+          },
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              cacheDirectory: true,
+              presets: [
+                ['env', {
+                  targets: {
+                    browsers: ['last 2 versions', 'safari >= 7'],
+                  },
+                }],
+              ],
+              plugins: [
+                ['istanbul', { exclude: ['**/*.spec.js'] }],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        exclude: /(node_modules)/,
+        use: ['html-loader', 'pug-html-loader']
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve('/'),
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  plugins: [
+  ],
+};
