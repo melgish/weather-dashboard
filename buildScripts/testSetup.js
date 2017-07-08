6c007244e912422dcc2abbcfac706de9bcec52a0
Nicholas J. Fiorello Jr <nfiorello@yahoo.com>
2017-07-07 22:54:00 -0400

    remove unused script

--- buildScripts/testSetup.js
+++
@@ -1,7 +0,0 @@
-// This file isn't transpiled, so must use CommonJS / ES5
-
-// Register babel to transpile before tests run
-require('babel-register')();
-
-// Disable webpack features that test framework doesn't understand.
-require.extensions['.css'] = function() {};
