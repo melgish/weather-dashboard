9ca75d6d548c50e8bc94571eb399264a1acd95dc
Nicholas J. Fiorello Jr <nfiorello@yahoo.com>
2017-07-07 23:12:16 -0400

    get apikey from APP_APIKEY env

--- server/accuweather/apikey.js
+++ server/accuweather/apikey.js
@@ -1,4 +1,7 @@
-const key = process.env.npm_package_config_apikey;
+process.env.APP_APIKEY =
+  process.env.APP_APIKEY || process.env.npm_package_config_apikey;
+
+const key = process.env.APP_APIKEY;
 if (!key) {
   console.error('accuweather API key not found.');
   console.error('npm config set @njf/weather-dashboard:apikey {key}');
