dccab23f65f9f41b545a8c5a735f8c8db483d3c8
Nicholas J. Fiorello Jr <nfiorello@yahoo.com>
2017-07-08 00:02:23 -0400

    fixed open calls for localhost

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -14,7 +14,7 @@ import api from '../server/api';
 
 const app = express();
 const port = process.env.APP_PORT || 3000
-const host = process.env.APP_HOST || '0.0.0.0';
+const host = process.env.APP_HOST || 'localhost';
 const logLevel = process.env.APP_LOGLEVEL || 'dev';
 
 if (logLevel !== 'none') {
